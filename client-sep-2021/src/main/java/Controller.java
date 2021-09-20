9629c3dcd9ec37cb79afed85480c34e047f57320
Михаил Левин <mikelevin@yandex-team.ru>
2021-09-20 22:00:31 +0300

    Lesson 2. Nio

--- client-sep-2021/src/main/java/Controller.java
+++ client-sep-2021/src/main/java/Controller.java
@@ -1,9 +1,16 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -19,21 +26,43 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Controller implements Initializable {
 
+    private static String ROOT_DIR = "client-sep-2021/root";
+    private static byte[] buffer = new byte[1024];
     public ListView<String> listView;
     public TextField input;
     private DataInputStream is;
     private DataOutputStream os;
 
     public void send(ActionEvent actionEvent) throws Exception {
-        String msg = input.getText();
+        String fileName = input.getText();
         input.clear();
-        os.writeUTF(msg);
-        os.flush();
+        sendFile(fileName);
+    }
+
+    private void sendFile(String fileName) throws IOException {
+        Path file = Paths.get(ROOT_DIR, fileName);
+        if (Files.exists(file)) {
+            long size = Files.size(file);
+
+            os.writeUTF(fileName);
+            os.writeLong(size);
+
+            InputStream fileStream = Files.newInputStream(file);
+            int read;
+            while ((read = fileStream.read(buffer)) != -1) {
+                os.write(buffer, 0, read);
+            }
+            os.flush();
+        } else {
+            os.writeUTF(fileName);
+            os.flush();
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
+            fillFilesInCurrentDir();
             Socket socket = new Socket("localhost", 8189);
             is = new DataInputStream(socket.getInputStream());
             os = new DataOutputStream(socket.getOutputStream());
@@ -42,7 +71,7 @@ public class Controller implements Initializable {
                     while (true) {
                         String msg = is.readUTF();
                         log.debug("received: {}", msg);
-                        Platform.runLater(() -> listView.getItems().add(msg));
+                        Platform.runLater(() -> input.setText(msg));
                     }
                 } catch (Exception e) {
                     log.error("exception while read from input stream");
@@ -53,5 +82,20 @@ public class Controller implements Initializable {
         } catch (IOException ioException) {
             log.error("e=", ioException);
         }
+    }
+
+    private void fillFilesInCurrentDir() throws IOException {
+        listView.getItems().clear();
+        listView.getItems().addAll(
+                Files.list(Paths.get(ROOT_DIR))
+                    .map(p -> p.getFileName().toString())
+                    .collect(Collectors.toList())
+        );
+        listView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2) {
+                String item = listView.getSelectionModel().getSelectedItem();
+                input.setText(item);
+            }
+        });
     }
 }
