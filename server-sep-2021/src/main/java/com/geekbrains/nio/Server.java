08ae7d74a911fc212bfc4fca056137a05385fd63
Александр Тимонин <GrAleen@ya.ru>
2021-09-27 17:18:24 +0300

    Выполнил работу по уроку, остался бак с перескакиванием на новую стоку, но работает. Закончу работу позже...

--- server-sep-2021/src/main/java/com/geekbrains/nio/Server.java
+++ server-sep-2021/src/main/java/com/geekbrains/nio/Server.java
@@ -1,5 +1,7 @@
 package com.geekbrains.nio;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -8,15 +10,21 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@Slf4j
 public class Server {
 
     private ServerSocketChannel serverChannel;
     private Selector selector;
     private ByteBuffer buffer;
+    private static Path ROOT = Paths.get("server-sep-2021","root");
 
     public Server() throws IOException {
 
@@ -24,6 +32,7 @@ public class Server {
         serverChannel = ServerSocketChannel.open();
         selector = Selector.open();
         serverChannel.bind(new InetSocketAddress(8189));
+        log.debug("server started");
         serverChannel.configureBlocking(false);
         serverChannel.register(selector, SelectionKey.OP_ACCEPT);
 
@@ -72,13 +81,58 @@ public class Server {
             }
             buffer.clear();
         }
-        String message = msg.toString();
-        channel.write(ByteBuffer.wrap(("[" + LocalDateTime.now() + "] " + message).getBytes(StandardCharsets.UTF_8)));
+        String message = msg.toString().trim();
+        log.debug("received  {}",msg);
+        if(message.startsWith("ls")){
+            channel.write(ByteBuffer.wrap(getFilesList().getBytes(StandardCharsets.UTF_8)));
+        }else if(message.startsWith("cat")){
+            String[] splitMessage =message.split(" ");
+            if(splitMessage.length<2){
+                log.debug("wrong command");
+                channel.write(ByteBuffer.wrap("\n wrong command pls enter fileName ".getBytes(StandardCharsets.UTF_8)));
+
+            }else {
+                String fileName = splitMessage[1];
+                channel.write(ByteBuffer.wrap(getFdataAsString(fileName).getBytes(StandardCharsets.UTF_8)));
+            }
+
+        }else {
+            channel.write(ByteBuffer.wrap(("\n wrong command pls choos cat or ls").getBytes(StandardCharsets.UTF_8)));
+            log.debug("wrong command");
+        }
+
+
+        channel.write(ByteBuffer.wrap(("[" + LocalDateTime.now() + "] " + message + "\n").getBytes(StandardCharsets.UTF_8)));
     }
 
     private void handleAccept(SelectionKey key) throws IOException {
         SocketChannel channel = serverChannel.accept();
         channel.configureBlocking(false);
         channel.register(selector, SelectionKey.OP_READ);
+        log.debug("client accepted");
+    }
+    private String getFilesList() throws IOException {
+        return Files.list(ROOT)
+                .map(this::fileOrDir)
+                .collect(Collectors.joining("\n")).toString() + "\n";
+    }
+    private String fileOrDir(Path path){
+        if(Files.isDirectory(path)){
+            return String.format("%3s%s" ,"[] " ,path.getFileName().toString() );
+
+        }else {
+            return String.format("%3s%s" ,"<> " ,path.getFileName().toString() );
+        }
+
+    }
+    private String getFdataAsString (String fileName) throws IOException {
+        if(Files.isDirectory(ROOT.resolve(fileName))){
+            return " {Error} cannot cat applied to " + fileName;
+        }else {
+
+            byte[] bytes = Files.readAllBytes(ROOT.resolve(fileName));
+            return new String(bytes);
+
+        }
     }
 }
